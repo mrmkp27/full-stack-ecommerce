9dacadf80338e7b6348a273812b796937dd39256
mrmkp27 <manojprajapat2711@gmail.com>
2025-01-12 22:26:41 +0530

    config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173},
+  server: { port: 5173 },
   build: {
     rollupOptions: {
-      external: ['react-router-dom','react-toastify'],
+      external: ['react-router-dom'], // Remove react-toastify from external
     },
   },
-  css: {
-    preprocessorOptions: {
-      css: {
-        modules: true,
-      },
+  resolve: {
+    alias: {
+      'react-toastify': 'react-toastify',
     },
+  },
+  optimizeDeps: {
+    include: ['react-toastify'],
   },
 })
